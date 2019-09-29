06a94000c97b105e8dcfda3bb440f87f8b8a6eaa
Maicon Domingues <maikonbdomingues@gmail.com>
2019-09-29 09:20:46 -0300

    Shopping cart using ReactJS with Redux and Redux Saga

---
+++ src/store/models/cart/reducer.js
@@ -0,0 +1,34 @@
+import produce from 'immer';
+
+export default function cart(state = [], action) {
+  switch(action.type) {
+    case '@cart/ADD_SUCCESS':
+    // validacao para nao duplicar o item dentro do carrinho
+    return produce(state, draft => {
+      const { product }= action;
+      draft.push(product);
+    });
+    // remove item do carrinho
+    case '@cart/REMOVE':
+    return produce(state, draft => {
+      const productIndex = draft.findIndex(p => p.id === action.id);
+      if (productIndex >= 0) {
+        draft.splice(productIndex, 1);
+      }
+    }); 
+
+    // aumenta-diminui itens no carrinho
+    case '@cart/UPDATE_AMOUNT_SUCCESS': {      
+      return produce(state, draft => {
+        const productIndex = draft.findIndex(p => p.id === action.id);
+        if (productIndex >= 0) {
+          draft[productIndex].amount = Number(action.amount);
+        }
+      }); 
+      
+    }
+
+    default:
+      return state;
+  }
+}
